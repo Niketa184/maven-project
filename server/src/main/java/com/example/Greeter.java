628b4fc92c42b35a963650e7c8b2be7b5e427625
Niketa <kulshresthaniketa@gmail.com>
2020-01-09 23:05:44 +0530

    fix checkstyle changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,7 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
